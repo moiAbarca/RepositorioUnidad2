a74353b8c4499a3953d5145a9c8a42756a95f32d
mOi <moisesabarca@gmail.com>
2016-05-04 20:06:54 -0300

    4 de mayo

---
+++ app/WelcomeModule/controller/FormValidationController.js
@@ -0,0 +1,10 @@
+/**
+ * Created by mOi on 02-05-16.
+ */
+
+//generar nuestro módulo
+var app = angular.module('LoginFomrModule', []);
+app.controller('LoginFormController',function ($scope, $http)
+{
+    
+});
