5cb5c24ae4d1e97ec3ba68e3be71a39c6486334a
mOi <moisesabarca@gmail.com>
2016-05-04 20:36:30 -0300

    Final de la clase

--- app/WelcomeModule/controller/FormValidationController.js
+++ app/WelcomeModule/controller/FormValidationController.js
@@ -6,5 +6,13 @@
 var app = angular.module('LoginFomrModule', []);
 app.controller('LoginFormController',function ($scope, $http)
 {
-    
+    //modelo, entidad generada para sustentar la vista
+    $scope.formModel = {};
+
+    //función que llama al submit
+    $scope.onSubmit = function () {
+      console.log("Llegué, Primer submit con angularJS!");
+        console.log($scope.formModel);
+        //más adelante haremos...
+    };
 });
